26e675d91e07a5d16a5b90f7a25ffc834dd9d0fa
Brandon Kearby <brandon.kearby+github@gmail.com>
2017-08-07 14:31:55 -0500

    set timeout to 10ms

--- src/main/java/com/symantec/cpe/analytics/kafka/ClusterMonitorRunnable.java
+++ src/main/java/com/symantec/cpe/analytics/kafka/ClusterMonitorRunnable.java
@@ -27,7 +27,7 @@ public class ClusterMonitorRunnable implements Runnable {
 
     private static final String CONSUMER_OFFSETS_TOPIC = "__consumer_offsets";
     private static final byte[] lock = new byte[0];
-    public static final long POLL_TIMEOUT = TimeUnit.SECONDS.toMillis(1);
+    public static final long POLL_TIMEOUT = 10L;
 
     private boolean running;
     private KafkaMonitorConfiguration kafkaMonitorConfiguration;
