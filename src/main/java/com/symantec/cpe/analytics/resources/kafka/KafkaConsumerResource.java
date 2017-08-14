6876f46626268578ba46c6df06912de05d2db899
Brandon Kearby <brandon.kearby+github@gmail.com>
2017-08-14 11:05:46 -0500

    Adding new Resource for Consumer

---
+++ src/main/java/com/symantec/cpe/analytics/resources/kafka/KafkaConsumerResource.java
@@ -0,0 +1,127 @@
+package com.symantec.cpe.analytics.resources.kafka;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.symantec.cpe.analytics.core.ResponseMessage;
+import com.symantec.cpe.analytics.core.kafka.KafkaOffsetMonitor;
+import com.symantec.cpe.analytics.core.kafka.KafkaTopicMonitor;
+import com.symantec.cpe.analytics.kafka.*;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+@Path("/kafka")
+@Consumes(MediaType.APPLICATION_JSON)
+public class KafkaResource {
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaResource.class);
+    private static final Function<String, Topic> STRING_TO_TOPIC_FUNCTION = new Function<String, Topic>() {
+        @Nullable
+        @Override
+        public Topic apply(@Nullable String input) {
+            return new Topic(input);
+        }
+    };
+    private ClusterMonitorService clusterMonitorService;
+
+    public KafkaResource(ClusterMonitorService clusterMonitor) {
+        this.clusterMonitorService = clusterMonitor;
+    }
+
+    @Path("/offset")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_HTML})
+    public Response getKafkaConsumerOffset(@DefaultValue("json") @QueryParam("outputType") String outputType) {
+        String output;
+        String responseType = MediaType.APPLICATION_JSON;
+        try {
+            List<KafkaOffsetMonitor> kafkaOffsetMonitors = clusterMonitorService.getKafkaOffsetMonitors();
+            if (outputType.equals("html")) {
+                responseType = MediaType.TEXT_HTML;
+                output = KafkaConsumerOffsetFormatter.htmlOutput(kafkaOffsetMonitors);
+            } else {
+                output = KafkaConsumerOffsetFormatter.jsonOutput(kafkaOffsetMonitors);
+            }
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ResponseMessage("Error Occurred during processing")).type(responseType).build();
+        }
+        return Response.status(Response.Status.OK).entity(output).type(responseType).build();
+    }
+
+    @Path("/topics/{topic}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getKafkaTopics(@PathParam("topic") @DefaultValue("_all") String topic) {
+        String responseType = MediaType.APPLICATION_JSON;
+        try {
+            List<KafkaTopicMonitor> kafkaOffsetMonitors;
+            kafkaOffsetMonitors = clusterMonitorService.getKafkaTopicMonitors(topic);
+
+            return Response.status(Response.Status.OK).entity(kafkaOffsetMonitors).type(responseType).build();
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ResponseMessage("Error Occurred during processing")).type(responseType).build();
+        }
+    }
+
+    private enum SeekTo {
+        beginning,
+        end
+    }
+
+    @Path("/consumer/{consumerGroup}/seek")
+    @PUT
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response seekToBeginning(@PathParam("consumerGroup") String consumerGroup, @NotNull @QueryParam("to") String to, @DefaultValue("_all") @QueryParam("topics") String topics) {
+        String responseType = MediaType.APPLICATION_JSON;
+        try {
+            if (org.apache.commons.lang3.StringUtils.isEmpty(to)) {
+                throw new IllegalStateException("Missing required request parameter: 'to'. Valid values: " + Joiner.on(", ").join(SeekTo.values()));
+            }
+            ClusterState clusterState = clusterMonitorService.getClusterState();
+            ConsumerGroupState consumerGroupState = clusterState.get(new ConsumerGroup(consumerGroup));
+            String[] selectedTopics = StringUtils.split(topics, ',');
+            Set<Topic> topicSet;
+            if (selectedTopics.length == 1 && selectedTopics[0].equals("_all")) {
+                topicSet = consumerGroupState.getTopics();
+            }
+            else {
+                topicSet = Sets.newHashSet(Lists.transform(Arrays.asList(selectedTopics), STRING_TO_TOPIC_FUNCTION));
+            }
+
+            List messages = new ArrayList();
+            for (Topic topic : topicSet) {
+                if (SeekTo.beginning.name().equalsIgnoreCase(to)) {
+                    messages = clusterMonitorService.seekToBeginning(consumerGroup, topic.getName());
+                }
+                else if (SeekTo.end.name().equalsIgnoreCase(to)) {
+                    messages = clusterMonitorService.seekToEnd(consumerGroup, topic.getName());
+                }
+                else {
+                    long timeInMs = Long.parseLong(to);
+                    messages = clusterMonitorService.seek(consumerGroup, topic.getName(), timeInMs);
+                }
+            }
+
+            return Response.status(Response.Status.OK).type(responseType)
+                    .entity(messages)
+                    .build();
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ResponseMessage(e.getLocalizedMessage())).type(responseType).build();
+        }
+    }
+
+}
